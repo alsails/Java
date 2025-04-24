910c61898c8a9b598ba81df2c627d1e5c68beaf3
alsails <mash.pogonina2014@yandex.ru>
2025-04-24 10:36:34 +0300

    1 задача

---
+++ src/task1/Action.java
@@ -0,0 +1,86 @@
+package task1;
+
+import sql.InsertIntoTable;
+
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class Action {
+    public static double number1;
+    public static double number2;
+
+    static void getParam(Boolean isTwo) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Введите первое число: ");
+            try {
+                number1 = Double.parseDouble(sc.nextLine());
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("Некорректный ввод. Необходимо ввести число.");
+            }
+        }
+
+        if (isTwo) {
+            while (true) {
+                System.out.println("Введите второе число: ");
+                try {
+                    number2 = Double.parseDouble(sc.nextLine());
+                    break;
+                } catch (NumberFormatException e) {
+                    System.out.println("Некорректный ввод. Необходимо ввести число.");
+                }
+            }
+        }
+    }
+
+    public static void sum() throws SQLException {
+        Action.getParam(true);
+        double summ = number1 + number2;
+        InsertIntoTable.insertIntoTable(new Object[] {"Сложение", number1, number2, summ});
+        System.out.println(number1 + " + " + number2 + " = " + summ);
+    }
+
+    public static void minus() throws SQLException {
+        Action.getParam(true);
+        double minuss = number1 - number2;
+        InsertIntoTable.insertIntoTable(new Object[] {"Вычитание", number1, number2, minuss});
+        System.out.println(number1 + " - " + number2 + " = " + minuss);
+    }
+
+    public static void multi() throws SQLException {
+        Action.getParam(true);
+        double multii = number1 * number2;
+        InsertIntoTable.insertIntoTable(new Object[] {"Умножение", number1, number2, multii});
+        System.out.println(number1 + " * " + number2 + " = " + multii);
+    }
+
+    public static void div() throws SQLException {
+        Action.getParam(true);
+        double divv = number1 / number2;
+        InsertIntoTable.insertIntoTable(new Object[] {"Деление чисел", number1, number2, divv});
+        System.out.println(number1 + " / " + number2 + " = " + divv);
+    }
+
+    public static void del() throws SQLException {
+        Action.getParam(true);
+        double dell = number1 % number2;
+        InsertIntoTable.insertIntoTable(new Object[] {"Деление по модулю", number1, number2, dell});
+        System.out.println(number1 + " % " + number2 + " = " + dell);
+    }
+
+    public static void modul() throws SQLException {
+        Action.getParam(false);
+        double modull = Math.abs(number1);
+        InsertIntoTable.insertIntoTable(new Object[] {"Модуль", number1, 0d, modull});
+        System.out.println("Возведение числа " + number1 + " в модуль: " + modull);
+    }
+
+    public static void stepen() throws SQLException {
+        Action.getParam(true);
+        double stepenn = Math.pow(number1, number2);
+        InsertIntoTable.insertIntoTable(new Object[] {"Возведение в степень", number1, number2, stepenn});
+        System.out.println(number1 + " ^ " + number2 + " = " + stepenn);
+    }
+}
---
+++ src/task1/Main.java
@@ -0,0 +1,84 @@
+package task1;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Scanner;
+import config.Config;
+import sql.*;
+
+public class Main {
+    protected final static Scanner sc = new Scanner(System.in);
+    protected final static String mysqlurl = Config.DB_URL + Config.DB_NAME;
+    public static Connection conn;
+
+    static {
+        System.out.println("Введите название таблицы: ");
+    }
+
+    public static String tableName = sc.nextLine();
+
+    static {
+        System.out.println("Введите название Excel файла: ");
+    }
+
+    public static String fileName = sc.nextLine();
+
+    static {
+        try {
+            conn = DriverManager.getConnection(mysqlurl, Config.DB_USER, Config.DB_PASSWORD);
+            System.out.println("Подлкючение успешно!");
+        } catch(SQLException e) {
+            System.out.println("Ошибка подключения к БД: " + e.getMessage());
+        }
+    }
+
+    // Задаем колонки для SQL
+    public static final String[][] columns = {
+            {"action", "VARCHAR(50)"},
+            {"number_1", "DOUBLE(10, 2)"},
+            {"number_2", "DOUBLE(10, 2)"},
+            {"result", "DOUBLE(10, 2)"}
+    };
+
+
+    public static void main(String[] args) throws SQLException {
+        int x = 0;
+        String s = "";
+        while (!"11".equals(s)) {
+            System.out.println("---------------------------\n" +
+                    "1. Вывести все таблицы из MySQL\n" +
+                    "2. Создать таблицу в MySQL\n" +
+                    "3. Сложение чисел, результат сохранить в MySQL с последующим выводом в консоль\n" +
+                    "4. Вычитание чисел, результат сохранить в MySQL с последующим выводом в консоль\n" +
+                    "5. Умножение чисел, результат сохранить в MySQL с последующим выводом в консоль\n" +
+                    "6. Деление чисел, результат сохранить в MySQL с последующим выводом в консоль\n" +
+                    "7. Деление чисел по модулю (остаток), результат сохранить в MySQL с последующим выводом в консоль\n" +
+                    "8. Возведение числа в модуль, результат сохранить в MySQL с последующим выводом в консоль\n" +
+                    "9. Возведение числа в степень, результат сохранить в MySQL с последующим выводом в консоль\n" +
+                    "10. Сохранить все данные (вышеполученные результаты) из MySQL в Excel и вывести на экран\n" +
+                    "11. Выход");
+           s = sc.next();
+
+            try {
+                x = Integer.parseInt(s);
+            } catch (NumberFormatException e) {
+                System.out.println("Неверный формат ввода");
+            }
+
+            switch (x) {
+                case 1 -> ViewsTables.viewsTables();
+                case 2 -> CreateTable.createTable();
+                case 3 -> Action.sum();
+                case 4 -> Action.minus();
+                case 5 -> Action.multi();
+                case 6 -> Action.div();
+                case 7 -> Action.del();
+                case 8 -> Action.modul();
+                case 9 -> Action.stepen();
+                case 10 -> ExportToExcel.exportToExcel();
+            }
+
+        }
+    }
+}
